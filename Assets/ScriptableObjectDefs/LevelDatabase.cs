e04da45b75fcd3d7f2f0f519e4599d72be33ebe9
Thanik <thanik@gmail.com>
2021-01-29 23:48:54 +0000

    Added CreateAssetMenu on LevelDatabase

--- Assets/ScriptableObjectDefs/LevelDatabase.cs
+++ Assets/ScriptableObjectDefs/LevelDatabase.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class LevelDatabase : ScriptableObject
 {
     public List<LevelDefinition> levels;
