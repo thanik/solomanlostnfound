30b111593d597cd03e06830aff421d93fa78297a
Daniel <firepro20@gmail.com>
2021-01-29 22:18:22 +0000

    Updated GameManager and UIControllers
    removed psd files

--- Assets/Scripts/Controllers/GameManager.cs
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public enum Days { Monday, Tuesday, Wednesday, Thursday, Friday }
+    Days dayLevel;
+
+    // defines function and parameters if required
+    public delegate void OnSatisfactionUpdateHandler(int point);
+
+    // event to subsbribe to
+    public event OnSatisfactionUpdateHandler OnSatisfactionUpdated;
+
+    public int dayTime = 3;
+    
+    private float levelTime = 0;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        dayLevel = Days.Monday;
+        levelTime = dayTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // do timer stuff.. decrease satisfaction every x seconds trickle effect kind of
+        // update satisfaction by a point if item is returned +/-
+        // point based on action so can invoke whenever needed  -- can also be called when collider goes over wrong or right person
+        if (Input.GetKeyDown(KeyCode.W))
+            OnSatisfactionUpdated?.Invoke(1);
+        if (Input.GetKeyDown(KeyCode.S))
+            OnSatisfactionUpdated?.Invoke(-1);
+    }
+
+    public Days GetDay()
+    {
+        return dayLevel;
     }
 }
--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -1,18 +1,125 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : Singleton<UIController>
 {
+    public enum Satisfaction
+    {
+        EXTREMELYDISAPPOINTED = -3,
+        VERYDISAPPOINTED,
+        NOTSATISFIED,
+        NEUTRAL,
+        SLIGHTLYSATISFIED,
+        VERYSATISFIED,
+        EXTREMELYSATISFIED
+    }
+
+    const int spriteArraySize = 6;
+    public Image[] satisfactionSprites = new Image[spriteArraySize];
+    [SerializeField]
+    private Image satisfactionSprite;
+    private Satisfaction satisfactionState;
+    private int satisfactionScale = 0;
+
+    private void OnValidate()
+    {
+        if (satisfactionSprites.Length != spriteArraySize)
+        {
+            Debug.LogWarning("Don't change the 'satisfactionSprites' field's array size!");
+            Array.Resize(ref satisfactionSprites, spriteArraySize);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        satisfactionState = Satisfaction.NEUTRAL;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        GameManager.Instance.OnSatisfactionUpdated += UpdateSatisfaction;
     }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnSatisfactionUpdated -= UpdateSatisfaction;
+    }
+
+    // Event to Update Satisfaction
+    void UpdateSatisfaction(int point)
+    {
+        satisfactionScale += point;
+
+        if (satisfactionScale >= 3)
+        {
+            satisfactionScale = 3;
+            satisfactionState = Satisfaction.EXTREMELYSATISFIED;
+        }
+        else if (satisfactionScale <= -3)
+        {
+            satisfactionScale = -3;
+            satisfactionState = Satisfaction.EXTREMELYDISAPPOINTED;
+        }
+
+        if (satisfactionScale < -1 && satisfactionScale >= -2)
+        {
+            satisfactionState = Satisfaction.VERYDISAPPOINTED;
+        }
+        else if (satisfactionScale < 0 && satisfactionScale >= -1)
+        {
+            satisfactionState = Satisfaction.NOTSATISFIED;
+        }
+        else if (satisfactionScale < 1 && satisfactionScale >= 0)
+        {
+            satisfactionState = Satisfaction.NEUTRAL;
+        }
+        else if (satisfactionScale < 2 && satisfactionScale >= 1)
+        {
+            satisfactionState = Satisfaction.SLIGHTLYSATISFIED;
+        }
+        else if (satisfactionScale < 3 && satisfactionScale >= 2)
+        {
+            satisfactionState = Satisfaction.VERYSATISFIED;
+        }
+
+        // Update UI
+        UpdateSatisfactionSprite();
+    }
+
+    // Function to update sprite
+    void UpdateSatisfactionSprite()
+    {
+        switch (satisfactionState)
+        {
+            case Satisfaction.EXTREMELYDISAPPOINTED:
+                Debug.Log("Extremely Disappointed");
+                break;
+            case Satisfaction.VERYDISAPPOINTED:
+                Debug.Log("Very Disappointed");
+                break;
+            case Satisfaction.NOTSATISFIED:
+                Debug.Log("Not Satisfied");
+                break;
+            case Satisfaction.NEUTRAL:
+                Debug.Log("Neutral");
+                break;
+            case Satisfaction.SLIGHTLYSATISFIED:
+                Debug.Log("Slightly Satisfied");
+                break;
+            case Satisfaction.VERYSATISFIED:
+                Debug.Log("Very Satisfied");
+                break;
+            case Satisfaction.EXTREMELYSATISFIED:
+                Debug.Log("Extremely Satisfied");
+                break;
+            default:
+                break;
+        }
+    }
+
 }
