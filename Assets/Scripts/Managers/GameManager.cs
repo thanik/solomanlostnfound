77a9d0fa4eff87c746dbfb6f6d4076f69580b3ff
Thanik <thanik@gmail.com>
2021-01-30 18:15:18 +0000

    Add BlackScreen fading in GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+
+    public Image blackScreen;
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void FadeToBlack()
+    {
+        blackScreen.gameObject.SetActive(true);
+        blackScreen.DOFade(1f, 1f);
+    }
+
+    public void FadeOut()
+    {
+        blackScreen.DOFade(0f, 1f);
+        blackScreen.gameObject.SetActive(false);
+    }
 }
